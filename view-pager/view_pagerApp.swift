15ab72727bd74d12bbc24af70bfbc2afe4a030f7
MyFirstGitEver <fordevok@gmail.com>
2023-08-23 10:15:49 +0700

    Initial Commit

---
+++ view-pager/view_pagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  view_pagerApp.swift
+//  view-pager
+//
+//  Created by FVFH4069Q6L7 on 23/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct view_pagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ view-pagerTests/view_pagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  view_pagerTests.swift
+//  view-pagerTests
+//
+//  Created by FVFH4069Q6L7 on 23/08/2023.
+//
+
+import XCTest
+@testable import view_pager
+
+final class view_pagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
